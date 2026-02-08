ce10e6b2e72f482ec1716391a1631bfb70e0ccf3
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-02-08 15:48:36 +0330

    Update board.h add based functions

--- includes/board.h
+++ includes/board.h
@@ -1,1 +1,21 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#define BOARD_SIZE 3
+#define PLAYER_X 'X'
+#define PLAYER_O 'O'
+
+typedef struct {
+    int player;
+    int computer;
+    int draw;
+} Score;
+
+void init_board(char board[BOARD_SIZE][BOARD_SIZE]);
+void print_board(char board[BOARD_SIZE][BOARD_SIZE], Score score);
+int check_win(char board[BOARD_SIZE][BOARD_SIZE], char player);
+int check_draw(char board[BOARD_SIZE][BOARD_SIZE]);
+int is_valid_move(char board[BOARD_SIZE][BOARD_SIZE], int row, int col);
+
+#endif
 
