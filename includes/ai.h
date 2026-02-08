cb214d8a19475fd41c5eb7f880b9c7a557e2abf0
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-02-08 15:48:57 +0330

    Update ai.h add based functions

--- includes/ai.h
+++ includes/ai.h
@@ -1,1 +1,10 @@
+#ifndef AI_H
+#define AI_H
+
+#include "board.h"
+
+void computer_move(char board[BOARD_SIZE][BOARD_SIZE], int difficulty);
+void player_move(char board[BOARD_SIZE][BOARD_SIZE]);
+
+#endif
 
